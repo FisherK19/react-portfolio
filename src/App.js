51180ba1ff538163fdcc5fa7e396175f16dd059e
kristie fisher <fisherkristie19@icloud.com>
2024-04-17 14:37:19 -0400

    adjusted APP.js, package files, debugging run build

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React  from "react";
-import Footer from './components/footer';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/header';
+import Footer from './components/footer';
+import About from './components/about';
+import Contact from './components/contact';
+import Portfolio from './components/portfolio';
+import Project from './components/project'; 
+import Resume from './components/resumeesume';
+import Navbar from './components/navbar'; 
 import './App.css';
 
 function App() {
-
-    return (
-        <div className="App">
-            <Header/>
-            <Footer/>
-        </div>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Navbar /> 
+        <Switch>
+          <Route path="/" exact component={Portfolio} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/project/:id" component={Project} /> 
+          <Route path="/resume" component={Resume} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
