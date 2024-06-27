ccfccdb9d3594d4b851371aca95b3febb9feb960
kristie fisher <fisherkristie19@icloud.com>
2024-06-27 13:08:18 -0400

    corrected project.js, now links navigate to github projects, websites and videos

--- src/components/project.js
+++ src/components/project.js
@@ -3,9 +3,6 @@ import '../App.css';
 import '../portfolio.css';
 
 function ProjectCards(props) {
-    const github = "https://github.com/your-username";
-    const deploy = "https://example.com";
-
     return (
         <div className={`grid-item gallery__item--${props.id}`}>
             <img src={props.image} alt={props.name} className="gallery__img"/>
@@ -13,14 +10,19 @@ function ProjectCards(props) {
                 <h5>{props.name}</h5>
             </div>
             <div className="dep-links">
-                <a href={github} target="_blank" rel="noopener noreferrer">
-                    <img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" className="project-icon" />
-                </a>
-                <a href={deploy} target="_blank" rel="noopener noreferrer">
-                    <img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" className="project-icon" />
-                </a>
+                {props.github && (
+                    <a href={props.github} target="_blank" rel="noopener noreferrer">
+                        <img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" className="project-icon" />
+                    </a>
+                )}
+                {props.deploy && (
+                    <a href={props.deploy} target="_blank" rel="noopener noreferrer">
+                        <img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" className="project-icon" />
+                    </a>
+                )}
             </div>
         </div>
     );
 }
+
 export default ProjectCards;
